a59eb4a0f085d13f5182b81fb8fd15487e2df163
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-08-15 00:25:12 +0200

    Create random sentence for HomeCitySentence component

--- app/components/HomeCitySentences.tsx
+++ app/components/HomeCitySentences.tsx
@@ -2,20 +2,28 @@ import { Text, View } from "react-native";
 import { WeatherInterface } from "../types/weatherInterfaces";
 
 import dataCodeCountry from "../services/data/dataCodeCountry.json";
+import dataSentences from "../services/data/dataSentences.json";
 
 import stylesHomeCity from "../styles/styleHomeCity";
+import { useEffect, useState } from "react";
 
 export default function HomeCitySentences({ data } :{data: WeatherInterface}){
+    const codeCountry: Record<string, string> = dataCodeCountry;
+    const sentences: Record<string, string[]> = dataSentences;
+
+    const [randomIndex, setRandomIndex] = useState(Math.floor(Math.random() * 3));
 
     const cityFromArrondissement = (cityName : string) => {
         const deleteSpeChart = cityName.replace(/'/g, " ");
         const breakSentence = deleteSpeChart.split(" ");
     
         return breakSentence.includes("Arrondissement") ? breakSentence.slice(2).join(" ") : cityName;
-      };
+    };
+  
+    useEffect(() => {
+        setRandomIndex(Math.floor(Math.random() * 3));
+    }, [data]);  
 
-      const codeCountry: Record<string, string> = dataCodeCountry;
-    
     return (
         <>
             <View>
@@ -23,7 +31,7 @@ export default function HomeCitySentences({ data } :{data: WeatherInterface}){
                 <Text style={stylesHomeCity.country}>{codeCountry[data.sys.country]}</Text>
             </View>
             <View style={stylesHomeCity.conatainerSentences}>
-                <Text style={stylesHomeCity.sentences}>It's 10 AM, and the weather isn't very nice today. Bring an umbrella if you go out.</Text>
+                <Text style={stylesHomeCity.sentences}>{sentences[data.weather[0].main][randomIndex]}</Text>
             </View>
         </>
     )
