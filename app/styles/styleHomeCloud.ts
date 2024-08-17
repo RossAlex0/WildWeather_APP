2574752eb30fe842346589d800f99035105cf339
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-08-17 11:23:10 +0200

    Added CSS for component HomeCloud

--- app/styles/styleHomeCloud.ts
+++ app/styles/styleHomeCloud.ts
@@ -2,7 +2,31 @@ import { StyleSheet } from 'react-native';
 import colors from './colors';
 
 const stylesHomeCloud = StyleSheet.create({
-
+    container:{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: -24
+    },
+    lottie:{
+        height: '52%', 
+        width: '60%',
+    },
+    containerTexte:{
+        position: 'absolute',
+        marginTop: '40%'
+    },
+    tempTexte:{
+        textAlign: 'center',
+        fontSize: 54,
+        fontWeight: '900',
+        color: colors.primaryColor
+    },
+    feelTexte:{
+        color: colors.primaryColor,
+        fontSize: 17,
+        fontWeight: '200'
+    }
 });
 
 export default stylesHomeCloud;
