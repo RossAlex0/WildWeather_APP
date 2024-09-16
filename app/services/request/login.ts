b09c89fe5e8f4b19436e73da3856227ad4df3dce
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-17 01:33:14 +0200

    Fixed request 💪

--- app/services/request/login.ts
+++ app/services/request/login.ts
@@ -3,10 +3,14 @@ import axios from "axios";
 
 const BACK_URL = Constants.expoConfig!.extra!.BACK_URL;
 
-export default async function login(mail: string, password: string) {
-  const user = { mail: mail, password: password };
+export default async function login(
+  mail: string,
+  password: string,
+  setter: (set: any) => void
+) {
+  const user = { mail, password };
   axios
     .post(`${BACK_URL}/login`, user)
-    .then((res) => console.info(res.data))
+    .then((res) => setter(res.data))
     .catch((err) => console.error(err.message));
 }
